b6e02e5654c0d42af47684db6e79e74a1aef2d7c
TJTforever <156537906+TJTforever@users.noreply.github.com>
2026-03-05 13:14:29 -0500

    Vuforia for Myself & Label Canvas
    Added in the label canvas & upgraded materials to urp. Also copied in vuforia from another project for myself

---
+++ Assets/Scripts/ShowCanvas.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowCanvas : MonoBehaviour
+{
+    public Camera arCamera;
+    public float threshold = 0.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 cameraVector = arCamera.transform.position - transform.position;
+        float dotProduct = Vector3.Dot(transform.forward, cameraVector.normalized);
+        if (dotProduct < -threshold)
+        {
+            transform.GetChild(0).gameObject.SetActive(true);
+        }
+        else
+        {
+            transform.GetChild(0).gameObject.SetActive(false);
+        }
+    }
+}
